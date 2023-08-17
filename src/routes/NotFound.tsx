c0e1956860918ecc782d5b0154d2a8bcc915432f
bonni-e <trial1bloom@gmail.com>
2023-08-17 17:03:07 +0900

    Add NotFound page

---
+++ src/routes/NotFound.tsx
@@ -0,0 +1,15 @@
+import { Button, HStack, Heading, Text, VStack } from "@chakra-ui/react";
+import { color } from "framer-motion";
+import { Link } from "react-router-dom";
+
+export default function NotFound() {
+    return (
+        <VStack minH={"100vh"} justifyContent={"center"} bg={"gray.100"}>
+            <Heading>Page not Found.</Heading>
+            <Text>It seems that you're lost.</Text>
+            <Link to={"/"}>
+                <Button variant={"solid"} colorScheme="linkedin">Go Home</Button>
+            </Link>
+        </VStack>
+    );
+}
